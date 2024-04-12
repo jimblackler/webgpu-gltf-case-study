aa90cf2343f93f2d270bc09248574f3a87b4b47a
Jim Blackler <jimblackler@google.com>
2024-04-12 22:22:24 +0100

    Simplify.

--- samples/js/gltf-demo.js
+++ samples/js/gltf-demo.js
@@ -24,13 +24,13 @@ const ShaderLocations = {
 };
 
 function createSolidColorTexture(device, r, g, b, a) {
-  const data = new Uint8Array([r * 255, g * 255, b * 255, a * 255]);
   const texture = device.createTexture({
     size: {width: 1, height: 1},
     format: 'rgba8unorm',
     usage: GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.COPY_DST
   });
-  device.queue.writeTexture({texture}, data, {}, {width: 1, height: 1});
+  device.queue.writeTexture({texture},
+      new Uint8Array([r * 255, g * 255, b * 255, a * 255]), {}, {width: 1, height: 1});
   return texture;
 }
 
